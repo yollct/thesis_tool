f139590337706cda3b3be5497e90c2073a279e5d
yollct <siuhuun@gmail.com>
2019-06-07 22:13:49 +0200

    add davidtermenrich

---
+++ myDAVID.py
@@ -0,0 +1,72 @@
+#!python
+# by courtesy of HuangYi @ 20110424
+
+def DAVIDtermenrich(listF, idType, bgF="/home/chit/Desktop/Thesis/data/bg_hs.txt", resF='', bgName = 'Background1',listName='List1', category = ''):
+    from suds.client import Client
+    import os
+   
+    if len(listF) > 0 and os.path.exists(listF):
+        inputListIds = ','.join(open(listF).read().split('\n'))
+        print ('List loaded.')     
+    else:
+        print ('No list loaded.')
+        raise
+
+    flagBg = False
+    if len(bgF) > 0 and os.path.exists(bgF):
+        inputBgIds = ','.join(open(bgF).read().split('\n'))
+        flagBg = True
+        print ('Use file background.')
+    else:
+        print ('Use default background.')
+        
+    url = 'https://david.ncifcrf.gov/webservice/services/DAVIDWebService?wsdl'
+    client = Client(url, location = "https://david.ncifcrf.gov/webservice/services/DAVIDWebService")
+    client.wsdl.services[0].setlocation('https://david.ncifcrf.gov/webservice/services/DAVIDWebService.DAVIDWebServiceHttpSoap11Endpoint/')
+    print ('User Authentication:',client.service.authenticate('ge75nij@mytum.de'))
+
+    listType = 0
+    print ('Percentage mapped(list):', client.service.addList(inputListIds,idType,listName,listType))
+    if flagBg:
+        listType = 1
+        print ('Percentage mapped(background):', client.service.addList(inputBgIds,idType,bgName,listType))
+
+    print ('Use categories:', client.service.setCategories(category))
+    #getTermClusteringReport
+    overlap=3
+    initialSeed = 3
+    finalSeed = 3
+    linkage = 0.5
+    kappa = 50
+    termClusteringReport = client.service.getTermClusterReport(overlap, initialSeed, finalSeed, linkage, kappa)
+    
+    #parse and print report
+    totalRows = len(termClusteringReport)
+    print ('Total clusters:',totalRows)
+    resF = listF + '.termClusteringReport.txt'
+    with open(resF, 'w') as fOut:
+        i = 0
+        fOut.write('Category\tTerm\tCount\t%\tPvalue\tGenes\tList Total\tPop Hits\tPop Total\tFold Enrichment\tBonferroni\tBenjamini\tFDR\tenrichmentscore\tcluster\n')
+        for simpleTermClusterRecord in termClusteringReport:
+            i = i+1
+            EnrichmentScore = simpleTermClusterRecord.score
+            for simpleChartRecord in  simpleTermClusterRecord.simpleChartRecords:
+                categoryName = simpleChartRecord.categoryName
+                termName = simpleChartRecord.termName
+                listHits = simpleChartRecord.listHits
+                percent = simpleChartRecord.percent
+                ease = simpleChartRecord.ease
+                Genes = simpleChartRecord.geneIds
+                listTotals = simpleChartRecord.listTotals
+                popHits = simpleChartRecord.popHits
+                popTotals = simpleChartRecord.popTotals
+                foldEnrichment = simpleChartRecord.foldEnrichment
+                bonferroni = simpleChartRecord.bonferroni
+                benjamini = simpleChartRecord.benjamini
+                FDR = simpleChartRecord.afdr
+                rowList =[categoryName,termName,str(listHits),str(percent),str(ease),Genes,str(listTotals),str(popHits),str(popTotals),str(foldEnrichment),str(bonferroni),str(benjamini),str(FDR),str(EnrichmentScore),str(i)]
+                fOut.write('\t'.join(rowList)+'\n')
+    print ('write file:', resF, 'finished!')
+
+if __name__ == __main__:
+    DAVIDtermenrich(listF = '/home/chit/Desktop/Thesis/results/01.06/clust7_72.txt', idType = 'ENSEMBL_GENE_ID', listName = 'clust7_72', category = 'abcd,BBID,BIOCARTA,COG_ONTOLOGY,INTERPRO,KEGG_PATHWAY,OMIM_DISEASE,PIR_SUPERFAMILY,SMART,SP_PIR_KEYWORDS,UP_SEQ_FEATURE,GOTERM_MF_FAT,GOTERM_CC_FAT,GOTERM_BP_FAT')
