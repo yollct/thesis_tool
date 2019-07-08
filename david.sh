870beb783622686f6421dbf9615c5b2b431ad219
Chit Tong Lio <chit@reposrv01.exbio.wzw.tum.de>
2019-07-08 16:50:55 +0200

    work on server

--- david.sh
+++ david.sh
@@ -6,18 +6,18 @@ read -p 'Enter the name of the cluster file(c1_c2): ' clusters
 read -p "Enter the DAVID analysis (termenrich/chart): " job
 if [ "$clusters" == "all" ]
 then
-    IFS=$"\n" read -d " " -r -a all_clusters  < "/home/chit/Desktop/Thesis/results/$data/all_clusters.txt"
+    IFS=$"\n" read -d " " -r -a all_clusters  < "/data/home/students/chit/Desktop/Thesis/results/$data/all_clusters.txt"
     for all_cluster in $all_clusters
     do
         python txt2csv.py $data $all_cluster
         echo Cluster $all_cluster file is converted to txt.
 
-        cd /home/chit/PythonClient-1.1/PythonClient
+        cd /data/home/students/chit/PythonClient-1.1/PythonClient
         case $job in
             "termenrich") python DAVIDtermenrich.py $data $all_cluster ;;              
             "chart") python DAVIDenrich.py $data $all_cluster ;;
         esac
-        cd /home/chit/Desktop/Thesis/thesis_tool
+        cd /data/home/students/chit/Thesis/thesis_tool
         echo Finished running Cluster $all_cluster.
     done
 else
@@ -26,12 +26,12 @@ else
         python txt2csv.py $data $cluster
         echo Cluster $cluster file is converted to txt.
 
-        cd /home/chit/PythonClient-1.1/PythonClient
+        cd /data/home/students/chit/PythonClient-1.1/PythonClient
         case $job in
             "termenrich") python DAVIDtermenrich.py $data $cluster ;;              
             "chart") python DAVIDenrich.py $data $cluster ;;
         esac
-        cd /home/chit/Desktop/Thesis/thesis_tool
+        cd /data/home/students/chit/Thesis/thesis_tool
         echo Finished running Cluster $cluster.
 done
 fi
