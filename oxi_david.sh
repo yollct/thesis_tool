3711f3a06302d8af925bc18754a45f1c1fbe4fef
yollct <siiuyo_huun@live.com>
2019-08-14 13:23:33 +0200

    change

--- oxi_david.sh
+++ oxi_david.sh
@@ -2,11 +2,11 @@
 ##term enrichment analysis for oxidation/repair related gene
 read -p "Enter the name of the results folder: " data
 
-python oxi_david.py $data
+python3 oxi_david.py $data
 
 cd /home/chit/PythonClient-1.1/PythonClient
 
-python3.6 oxi_termdavid.py $data 
+python oxi_termdavid.py $data 
 echo DAVID finished!
 
 cd /home/chit/Desktop/Thesis/thesis_tool
