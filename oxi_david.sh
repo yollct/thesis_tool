94371568a3f83d669934da6ee70098dc7ffbbf44
yollct <siiuyo_huun@live.com>
2019-08-14 13:44:58 +0200

    change address

--- oxi_david.sh
+++ oxi_david.sh
@@ -4,10 +4,10 @@ read -p "Enter the name of the results folder: " data
 
 python3 oxi_david.py $data
 
-cd /home/chit/PythonClient-1.1/PythonClient
-
+cd /data/home/students/chit/myDAVIDAPI/PythonClient
+h
 python oxi_termdavid.py $data 
 echo DAVID finished!
 
-cd /home/chit/Desktop/Thesis/thesis_tool
+cd /data/home/students/chit/Thesis/thesis_tool
 
