e5aa05c9f903bb53bbd6af1e95c2ad4d81384185
yollct <siiuyo_huun@live.com>
2019-08-14 15:05:29 +0200

    typo

--- oxi_david.sh
+++ oxi_david.sh
@@ -2,10 +2,10 @@
 ##term enrichment analysis for oxidation/repair related gene
 read -p "Enter the name of the results folder: " data
 
-python3 oxi_david.py $data
+python oxi_david.py $data
 
 cd /data/home/students/chit/myDAVIDAPI/PythonClient
-h
+
 python oxi_termdavid.py $data 
 echo DAVID finished!
 
