9c00a14b6920e414b5998fbd79036ff698f07b80
yollct <ct7298@gmail.com>
2019-12-11 02:20:31 +0100

    bracket

--- combine_cluster.py
+++ combine_cluster.py
@@ -14,18 +14,21 @@ hlo_genes = []
 for x in clusters:
     cluster = "Cluster "+ x
     get_genes = cluster_table.loc[cluster_table.cluster=="Cluster 82"]['object'].to_list()
-    if len(hlo_genes)>3000:
-        num_to_pick = len(hlo_genes)//3000
-        for j in range(1,num_to_pick+1):
-            random_genes = random.sample(hlo_genes, 2999)
-            with open('/nfs/home/students/chit/Thesis/results/{}/highlogenes{}.txt'.format(data,j),"w") as f:
-                for i in random_genes:
-                    f.write(i+"\n")
-            f.close()
-    else:
-        with open('/nfs/home/students/chit/Thesis/results/{}/highlogenes1.txt'.format(data),"w") as f:
-                for i in hlo_genes:
-                    f.write(i+"\n")
+    for y in get_genes:
+        hlo_genes.append(y)
+
+if len(hlo_genes)>3000:
+    num_to_pick = len(hlo_genes)//3000
+    for j in range(1,num_to_pick+1):
+        random_genes = random.sample(hlo_genes, 2999)
+        with open('/nfs/home/students/chit/Thesis/results/{}/highlogenes{}.txt'.format(data,j),"w") as f:
+            for i in random_genes:
+                f.write(i+"\n")
         f.close()
+else:
+    with open('/nfs/home/students/chit/Thesis/results/{}/highlogenes1.txt'.format(data),"w") as f:
+        for i in hlo_genes:
+            f.write(i+"\n")
+    f.close()
 
 print("The high log genes are saved in {} folder.".format(data))
