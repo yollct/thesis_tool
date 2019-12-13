b2b6e14055c06a010da9496a95c1ecd14c808c54
yollct <ct7298@gmail.com>
2019-12-13 16:03:52 +0100

    get all connect clusters

--- reactomepa.R
+++ reactomepa.R
@@ -6,7 +6,7 @@ suppressMessages(library(DESeq2))
 
 arg <- commandArgs()
 data <- arg[6]
-scale <- arg[7]
+scale <- as.numeric(arg[7])
 reactomeplots <- function(data,scale){
 genes <- readLines(sprintf("/nfs/home/students/chit/Thesis/results/%s/highlogenes_ens.txt",data))
 entrez <- AnnotationDbi::select(org.Hs.eg.db,
@@ -16,7 +16,7 @@ entrez <- AnnotationDbi::select(org.Hs.eg.db,
 
 x <- enrichPathway(gene=entrez$ENTREZID,pvalueCutoff=0.05, readable=T)
 emapplot(x)
-ggsave(sprintf("/nfs/home/students/chit/Thesis/results/%s/%s_emapplot.pdf",data,data),device = "pdf", scale=scale)
+ggsave(sprintf("/nfs/home/students/chit/Thesis/results/%s/%s_emapplot.pdf",data,data),device = "pdf", scale= scale)
 cnetplot(x, categorySize="pvalue")
 ggsave(sprintf("/nfs/home/students/chit/Thesis/results/%s/%s_cnetplot.pdf",data,data),device = "pdf",scale = scale)
 }
