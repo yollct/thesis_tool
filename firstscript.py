e86a15bb0907fd0678ad2f52a7e69c169c025fa8
yollct <ct7298@gmail.com>
2019-12-13 15:03:17 +0100

    get all connect clusters

--- firstscript.py
+++ firstscript.py
@@ -8,41 +8,58 @@ import sys
 data=sys.argv[1]
 pvalue = float(sys.argv[2])
 
-nodes = pd.read_csv("/nfs/home/students/chit/Thesis/results/{}/cluster_table.csv".format(data))
-edge = pd.read_csv("/nfs/home/students/chit/Thesis/results/{}/connectivity_edge.csv".format(data))
+def get_edges(data, High_log_odd=True):
+    nodes = pd.read_csv("/nfs/home/students/chit/Thesis/results/{}/cluster_table.csv".format(data))
+    edge = pd.read_csv("/nfs/home/students/chit/Thesis/results/{}/connectivity_edge.csv".format(data))
 
-new_edge = edge[(edge['p']<=pvalue) & (edge['log-odds'].astype(float)>0.)]
+    if High_log_odd==True:
+        new_edge = edge[(edge['p']<=pvalue) & (edge['log-odds'].astype(float)>0.)]
+    else:
+        new_edge = edge[(edge['p']<=pvalue)]
+    return(new_edge)
+    
 
-interact = new_edge[['Cluster 1', 'Cluster 2']]
-interact.columns=['c1','c2']
+def connectivity_cluster(data, edges, high=True):
+    interact = new_edge[['Cluster 1', 'Cluster 2']]
+    interact.columns=['c1','c2']
 
-final = pd.DataFrame([interact.c1.str.split().str[1],
-             interact.c2.str.split().str[1]]).to_dict('list')
+    final = pd.DataFrame([interact.c1.str.split().str[1],
+                interact.c2.str.split().str[1]]).to_dict('list')
 
-highlo=[]
-for u,v in final.items():
-    highlo.append(v[0])
-    highlo.append(v[1])
+    highlo=[]
+    for u,v in final.items():
+        highlo.append(v[0])
+        highlo.append(v[1])
 
-highlogodd = list(set(highlo))
+    highlogodd = list(set(highlo))
 
-with open("/nfs/home/students/chit/Thesis/results/{}/all_connect.txt".format(data), 'w') as file:
-    for key, value in final.items():
-        file.write(value[0]+'_'+value[1]+'\n')
-    file.close()
+    if high=True:
+        with open("/nfs/home/students/chit/Thesis/results/{}/high_log_odd.txt".format(data), "w") as f:
+            for x in highlogodd:
+                f.write(x+'_')
+            f.close()
+    else:
+        with open("/nfs/home/students/chit/Thesis/results/{}/all_connect.txt".format(data), "w") as f:
+        for x in highlogodd:
+            f.write(x+'_')
+        f.close()
 
-with open("/nfs/home/students/chit/Thesis/results/{}/high_log_odd.txt".format(data), "w") as f:
-    for x in highlogodd:
-        f.write(x+'_')
+def get_all_clusters(data):
+##get all clusters
+    clusters = np.unique(nodes['cluster'])
+    clustersno = []
+    for c in clusters:
+        a = c.split(" ")
+        clustersno.append(a[1])
+
+    with open("/nfs/home/students/chit/Thesis/results/{}/all_clusters.txt".format(data), "w") as f:
+        for d in clustersno:
+            f.write(d+'\n')
     f.close()
 
-##get all clusters
-clusters = np.unique(nodes['cluster'])
-clustersno = []
-for c in clusters:
-    a = c.split(" ")
-    clustersno.append(a[1])
-
-with open("/nfs/home/students/chit/Thesis/results/{}/all_clusters.txt".format(data), "w") as f:
-    for d in clustersno:
-        f.write(d+'\n')
+if __name__ == "__main__":
+    high_edge = get_edges(data, High_log_odd=True)
+    all_edges = get_edges(data, High_log_odd=False)
+    connectivity_cluster(data, high_edge, high=True)
+    connectivity_cluster(data, all_edges, high=False)
+    get_all_clusters(data)
