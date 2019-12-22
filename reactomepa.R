3bfa0dc8f9f6f55c0442ec57f4d7f8c7360d696d
yollct <ct7298@gmail.com>
2019-12-22 13:07:08 +0100

    reactomepa

--- reactomepa.R
+++ reactomepa.R
@@ -19,11 +19,8 @@ entrez <- AnnotationDbi::select(org.Hs.eg.db,
 x <- enrichPathway(gene=entrez$ENTREZID,pvalueCutoff=0.05, readable=T)
 emapplot(x)
 ggsave(sprintf("/nfs/home/students/chit/Thesis/results/%s/%s_emapplot.pdf",data,data),device = "pdf", scale= scale)
-# cnetplot(x, categorySize="pvalue")
-# ggsave(sprintf("/home/chit/Desktop/Thesis/results/%s/%s_cnetplot.pdf",data,data),device = "pdf",scale = scale)
-
-dotplot(x, showCategory=30, color="pvalue", size="Count")
-ggsave(sprintf("/nfs/home/students/chit/Thesis/results/%s/%s_dotplot.pdf",data,data),device = "pdf", scale= scale)
+cnetplot(x, categorySize="pvalue")
+ggsave(sprintf("/home/chit/Desktop/Thesis/results/%s/%s_cnetplot.pdf",data,data),device = "pdf",scale = scale)
 
 xtable(data.frame(x), type="latex", file=sprintf("/nfs/home/students/chit/Thesis/results/%s/%s_reactomepa.pdf",data,data))
 
