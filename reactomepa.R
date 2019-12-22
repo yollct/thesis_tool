027097e9c0ddf699c4113d21a198c1b1a28d4d0f
yollct <ct7298@gmail.com>
2019-12-22 13:12:55 +0100

    reactomepa

--- reactomepa.R
+++ reactomepa.R
@@ -21,7 +21,7 @@ x <- enrichPathway(gene=entrez$ENTREZID,pvalueCutoff=0.05, readable=T)
 emapplot(x)
 ggsave(sprintf("/nfs/home/students/chit/Thesis/results/%s/%s_emapplot.pdf",data,data),device = "pdf", scale= scale)
 cnetplot(x, categorySize="pvalue")
-ggsave(sprintf("/home/chit/Desktop/Thesis/results/%s/%s_cnetplot.pdf",data,data),device = "pdf",scale = scale)
+ggsave(sprintf("/nfs/home/students/chit/Thesis/results/%s/%s_cnetplot.pdf",data,data),device = "pdf",scale = scale)
 
 xtable(data.frame(x), type="latex", file=sprintf("/nfs/home/students/chit/Thesis/results/%s/%s_reactomepa.txt",data,data))
 
